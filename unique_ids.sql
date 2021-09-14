859edbce6d7ed23f1ceafef3e096ffaa66f9c029
Matias Berretta <matiasberretta93@gmail.com>
2021-09-14 15:03:40 -0400

    Create unique_ids.sql

---
+++ unique_ids.sql
@@ -0,0 +1,8 @@
+# Create a unique ID for distinct conversations, where a conversation consists of two users sending each other messages
+# Clarification: user can be both the recipient or the sender of a message
+# (consisiting of a two users) from unique sender ids and recipient ids where each message has a unique identifier 
+
+| sender | receiver | message_id |
+| 1100 | 1200 | A12 | 
+| 1200 | 1100 | A23 | 
+| 1300 | 1200 | A33 | 
