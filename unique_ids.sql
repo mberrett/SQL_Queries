686472199db09d5c4254b8adcfb965e34738ab5b
Matias Berretta <matiasberretta93@gmail.com>
2021-09-16 13:10:54 -0400

    Basic Identifier

--- unique_ids.sql
+++ unique_ids.sql
@@ -1,8 +1,27 @@
 # Create a unique ID for distinct conversations, where a conversation consists of two users sending each other messages
-# Clarification: user can be both the recipient or the sender of a message
-# (consisiting of a two users) from unique sender ids and recipient ids where each message has a unique identifier 
+# Users can send and receive messages; their user id's are unique and fixed, regardless of whether they're sending or receiving a message 
+# Each message has a unique id; no two messages have the same id
 
-| sender | receiver | message_id |
-| 1100 | 1200 | A12 | 
-| 1200 | 1100 | A23 | 
-| 1300 | 1200 | A33 | 
+SELECT * FROM app_messages
+LIMIT 4
+
+# OUTPUT
+| sender | receiver | message_id | timestamp         |
+| 1100   | 1200     | M0021      | 12-01-01 05:03:23 |
+| 1200   | 1100     | M0023      | 12-01-01 05:04:56 |
+| 1300   | 1200     | M0024      | 12-01-01 05:05:31 |
+| 1200   | 1400     | M0025      | 12-01-01 06:45:18 |
+
+### Create Unique identifier for each conversation
+
+SELECT CASE WHEN sender > receiver THEN CONCAT(sender::varchar, receiver::varchar) 
+       ELSE CONCAT(receiver::varchar, sender::varchar) END AS convo_id, message_id
+FROM app_messages
+LIMIT 4
+
+# OUTPUT
+| convo_id     | message_id | timestamp         |
+| 12001100     | M0021      | 12-01-01 05:03:23 |
+| 12001100     | M0023      | 12-01-01 05:04:56 |
+| 13001200     | M0024      | 12-01-01 05:05:31 |
+| 14001200     | M0025      | 12-01-01 06:45:18 |
