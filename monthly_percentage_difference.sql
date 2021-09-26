96d2c0ec89ae2c6044bc7212d36355fd0a153358
Matias Berretta <matiasberretta93@gmail.com>
2021-09-26 14:22:34 -0400

    Update monthly_percentage_difference.sql

--- monthly_percentage_difference.sql
+++ monthly_percentage_difference.sql
@@ -1,7 +1,7 @@
-# Monthly Percentage Difference
-# Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
+-- Monthly Percentage Difference
+-- Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
 
-# sf_transactions table
+-- sf_transactions table
 
 id          | int
 created_at  | datetime
@@ -10,14 +10,12 @@ purchase_id | int
 
 
 # Long Format Answer (readibility)
-
-
 WITH month_level as (SELECT TO_CHAR(created_at, 'YYYY-MM') as year_month, 
-                    sum(value) as month_rev
+                     sum(value) as month_rev
 
                     FROM sf_transactions
 
-                    GROUP BY TO_CHAR(created_at, 'YYYY-MM')), # or GROUP BY 1
+                    GROUP BY TO_CHAR(created_at, 'YYYY-MM')), -- or GROUP BY 1
                     
                     
 lagged as (SELECT year_month, month_rev, LAG(month_rev) OVER(ORDER BY year_month asc) as prev_month_rev
@@ -28,9 +26,13 @@ FROM lagged
 
 
 # Condensed Format Answer
-
 SELECT TO_CHAR(created_at, 'YYYY-MM') as year_month,
-ROUND(((SUM(value) - LAG(SUM(value)) OVER(ORDER by TO_CHAR(created_at, 'YYYY-MM')))/LAG(SUM(value)) OVER(ORDER BY to_char(created_at, 'YYYY-MM')))*100,2) as perc_diff
+
+     ROUND(
+      ((SUM(value) - LAG(SUM(value)) OVER(ORDER by TO_CHAR(created_at, 'YYYY-MM'))) /
+       LAG(SUM(value)) OVER(ORDER BY to_char(created_at, 'YYYY-MM'))) * 100
+       , 2) as perc_diff
+       
 FROM
 sf_transactions
-GROUP by TO_CHAR(created_at, 'YYYY-MM') # or GROUP BY 1
+GROUP by TO_CHAR(created_at, 'YYYY-MM') -- or GROUP BY 1
